c741666a4cbcf135d17d8bc9dfbb7cd765a04783
Rob Sawyer <robksawyer@gmail.com>
2019-06-12 22:23:51 -0700

    Added the GetToken func

---
+++ GetToken.go
@@ -0,0 +1,60 @@
+// Amazon Seller Utilities
+package utilities
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+type AmazonToken struct {
+	AccessToken       string        `json:"amazon_access_token"`
+	AccessTokenExpiry time.Duration `json:"amazon_accessToken_expiry"`
+}
+
+// GetToken
+// Very important important function that retrieves a token based on a profile id.
+// @param string profileID Profile id
+// @param string userID User id
+// @return AmazonToken
+func GetToken(profileID string, userID string) (token string, err error) {
+	payload := map[string]string{
+		"profileId": profileID,
+		"userId":    userID,
+	}
+	jsonValue, _ := json.Marshal(payload)
+	log.Output(1, string(jsonValue))
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		os.Getenv("GET_AMAZON_TOKEN_SERVICE_ADDRESS"),
+		bytes.NewBuffer(jsonValue),
+	)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+	defer req.Body.Close()
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", os.Getenv("GET_AMAZON_TOKEN_SERVICE_TOKEN"))
+
+	httpClient := http.Client{}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	var amazonToken AmazonToken
+
+	err = json.NewDecoder(resp.Body).Decode(&amazonToken)
+	if err != nil {
+		return "", err
+	}
+	resp.Body.Close()
+	req.Body.Close()
+	return amazonToken.AccessToken, nil
+}
