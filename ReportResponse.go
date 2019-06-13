d3553f767821b83d48415f558dbf97aefc845d57
Rob Sawyer <robksawyer@gmail.com>
2019-06-13 03:34:58 -0700

    Added ReportResponse

--- ReportResponse.go
+++ ReportResponse.go
@@ -1,15 +1,15 @@
 // Package utilities Amazon snapshot response
 package utilities
 
-// SnapshotResponse The Amazon Snapshot response
-type SnapshotResponse struct {
+// ReportResponse The Amazon Snapshot response
+type ReportResponse struct {
 	Title      string `json:"title"`
 	Type       string `json:"type"`
 	Properties struct {
-		SnapshotID struct {
+		ReportID struct {
 			Description string `json:"description"`
 			Type        string `json:"type"`
-		} `json:"snapshotId"`
+		} `json:"reportId"`
 		RecordType struct {
 			Description string `json:"description"`
 			Type        string `json:"type"`
@@ -30,10 +30,6 @@ type SnapshotResponse struct {
 			Description string `json:"description"`
 			Type        int    `json:"type"`
 		} `json:"fileSize"`
-		Expiration struct {
-			Description string `json:"description"`
-			Type        string `json:"type"`
-		} `json:"expiration"`
 	} `json:"properties"`
 	Code      string `json:"code"`
 	Details   string `json:"details"`
