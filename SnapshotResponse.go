1a30e6aa1a6126758f9be0e73c6afcc0e5a8aa04
Rob Sawyer <robksawyer@gmail.com>
2019-06-13 03:08:53 -0700

    Updated the request struct

--- SnapshotResponse.go
+++ SnapshotResponse.go
@@ -6,34 +6,34 @@ type SnapshotResponse struct {
 	Title      string `json:"title"`
 	Type       string `json:"type"`
 	Properties struct {
-		SnapshotID struct {
-			Description string `json:"description"`
-			Type        string `json:"type"`
-		} `json:"snapshotId"`
-		RecordType struct {
-			Description string `json:"description"`
-			Type        string `json:"type"`
-		} `json:"recordType"`
-		Status struct {
-			Description string `json:"description"`
-			Type        string `json:"type"`
-		} `json:"status"`
-		StatusDetail struct {
-			Description string `json:"description"`
-			Type        string `json:"type"`
-		} `json:"statusDetails"`
-		Location struct {
-			Description string `json:"description"`
-			Type        string `json:"type"`
-		} `json:"location"`
-		FileSize struct {
-			Description string `json:"description"`
-			Type        string `json:"type"`
-		} `json:"fileSize"`
-		Expiration struct {
-			Description string `json:"description"`
-			Type        string `json:"type"`
-		} `json:"expiration"`
+		// SnapshotID struct {
+		// 	Description string `json:"description"`
+		// 	Type        string `json:"type"`
+		// } `json:"snapshotId"`
+		// RecordType struct {
+		// 	Description string `json:"description"`
+		// 	Type        string `json:"type"`
+		// } `json:"recordType"`
+		// Status struct {
+		// 	Description string `json:"description"`
+		// 	Type        string `json:"type"`
+		// } `json:"status"`
+		// StatusDetail struct {
+		// 	Description string `json:"description"`
+		// 	Type        string `json:"type"`
+		// } `json:"statusDetails"`
+		// Location struct {
+		// 	Description string `json:"description"`
+		// 	Type        string `json:"type"`
+		// } `json:"location"`
+		// FileSize struct {
+		// 	Description string `json:"description"`
+		// 	Type        string `json:"type"`
+		// } `json:"fileSize"`
+		// Expiration struct {
+		// 	Description string `json:"description"`
+		// 	Type        string `json:"type"`
+		// } `json:"expiration"`
 	} `json:"properties"`
 	Code      string `json:"code"`
 	Details   string `json:"details"`
