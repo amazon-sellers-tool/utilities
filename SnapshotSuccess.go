ba96591ff225ef0be8cca3cb5a7e7d88bded2efc
Rob Sawyer <robksawyer@gmail.com>
2019-06-13 21:19:19 -0700

    Returned SnapshotSuccess as it was

--- SnapshotSuccess.go
+++ SnapshotSuccess.go
@@ -3,38 +3,30 @@ package utilities
 
 import (
 	"encoding/json"
-	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
 // SnapshotSuccess A success response from the API
-func SnapshotSuccess(body []byte) (*SnapshotResponse, error) {
-	var s = new(SnapshotResponse)
-	err := json.Unmarshal(body, &s)
-	if err != nil {
-		fmt.Println("whoops:", err)
+func SnapshotSuccess(w http.ResponseWriter, version int, result io.Reader) error {
+	var decodedBody SnapshotResponse
+	errDecode := json.NewDecoder(result).Decode(&decodedBody)
+	if errDecode != nil {
+		return SnapshotError(w, version, 404, errDecode)
 	}
-	return s, err
+	apiResponse := SnapshotAPIResponse{
+		Version: version,
+		Success: true,
+		Status:  200,
+		Results: decodedBody,
+		Error:   "",
+	}
+	apiResponseJSON, err := json.Marshal(apiResponse)
+	if err != nil {
+		log.Panic(err)
+		panic(err)
+	}
+	log.Output(1, string(apiResponseJSON))
+	return json.NewEncoder(w).Encode(apiResponse)
 }
-
-// SnapshotSuccess A success response from the API
-// func SnapshotSuccess(w http.ResponseWriter, version int, body []byte) error {
-// 	var decodedBody SnapshotResponse
-// 	errDecode := json.NewDecoder(result).Decode(&decodedBody)
-// 	if errDecode != nil {
-// 		return SnapshotError(w, version, 404, errDecode)
-// 	}
-// 	apiResponse := SnapshotAPIResponse{
-// 		Version: version,
-// 		Success: true,
-// 		Status:  200,
-// 		Results: decodedBody,
-// 		Error:   "",
-// 	}
-// 	apiResponseJSON, err := json.Marshal(apiResponse)
-// 	if err != nil {
-// 		log.Panic(err)
-// 		panic(err)
-// 	}
-// 	log.Output(1, string(apiResponseJSON))
-// 	return json.NewEncoder(w).Encode(apiResponse)
-// }
