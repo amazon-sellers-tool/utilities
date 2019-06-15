92436e91b2dbb8010e726a04c1d71b391a9d0370
Scott S <tommy9828@gmail.com>
2019-06-15 02:28:19 -0700

    에혀;

--- GetReportSuccess.go
+++ GetReportSuccess.go
@@ -4,17 +4,15 @@ package utilities
 import (
 	"encoding/json"
 	"io"
-	"log"
 	"net/http"
 )
 
 // GetReportSuccess A success response from the API
-func GetReportSuccess(w http.ResponseWriter, version int, result io.Reader) error {
+func GetReportSuccess(w http.ResponseWriter, version int, result io.Reader) GetReportResponse {
 	var decodedBody GetReportResponse
 	errDecode := json.NewDecoder(result).Decode(&decodedBody)
 	if errDecode != nil {
-		return GetReportError(w, version, 404, errDecode)
+		GetReportError(w, version, 404, errDecode)
 	}
-	log.Output(1, string(apiResponseJSON))
 	return decodedBody
 }
