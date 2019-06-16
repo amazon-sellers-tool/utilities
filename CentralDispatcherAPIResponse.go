5ddd0db8608aa7b62b27d7f25dad77f5d778838d
Rob Sawyer <robksawyer@gmail.com>
2019-06-16 01:32:28 -0700

    Bug fix

--- CentralDispatcherAPIResponse.go
+++ CentralDispatcherAPIResponse.go
@@ -3,9 +3,9 @@ package utilities
 
 // RequestSnapshotAPIResponse The Amazon Seller Tool response
 type RequestSnapshotAPIResponse struct {
-	Version int                     `json:"version"`
-	Success bool                    `json:"success"`
-	Status  int                     `json:"status"`
-	Results map[string] 						`json:"results"`
-	Error   string                  `json:"error"`
+	Version int               `json:"version"`
+	Success bool              `json:"success"`
+	Status  int               `json:"status"`
+	Results map[string]string `json:"results"`
+	Error   string            `json:"error"`
 }
